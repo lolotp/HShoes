80bd3718bce78f2ab39052c9fad1de8bf4f29c2f
Phuong <trinhp@garena.com>
2015-01-05 23:55:17 +0800

    Initial commit

---
+++ DataTypes.hs
@@ -0,0 +1,88 @@
+{-# LANGUAGE DeriveGeneric              #-}
+{-# LANGUAGE QuasiQuotes                #-}
+{-# LANGUAGE TemplateHaskell            #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE GADTs                      #-}
+{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE TypeFamilies               #-}
+
+module DataTypes (Shoe,
+                  ShoeRaw,
+                  migrateAll,
+                  shoeFromRawData,
+                  saveImageData,
+                  photo,
+                  color,
+                  size,
+                  description,
+                  queryShoe) where
+
+import           Control.Monad.IO.Class   (MonadIO)
+import           Data.Aeson               (FromJSON, ToJSON)
+import           Data.Text                (Text, pack)
+import           Data.Text.Encoding       (encodeUtf8)
+import           Data.ByteString          (ByteString)
+import           Data.ByteString.Base64   (decode)
+import           Data.Conduit             (($$), Source, yield)
+import           Data.ByteString.Base16   (encode)
+import           Database.Persist.Sql     (SqlPersistT)
+import           Data.Int                 (Int64)
+import           Control.Monad.Trans.Resource
+import           GHC.Generics
+import           Database.Persist
+import           Database.Persist.TH
+import qualified Data.Conduit.Binary as CB
+import qualified Crypto.Hash.SHA256 as SHA256
+import qualified Data.ByteString.Char8 as B
+
+share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
+Shoe
+    description Text
+    color       Text
+    size        Int
+    photoPath   Text
+    deriving Show
+|]
+
+data ShoeRaw =
+  ShoeRaw { description :: !Text
+          , color       :: !Text
+          , size        ::  Int
+          , photo       :: !Text
+  } deriving (Show, Generic)
+
+shoeFromRawData :: ShoeRaw -> FilePath -> Shoe
+shoeFromRawData rawShoeData shoeImageFile =
+    let (ShoeRaw description color size _) = rawShoeData in
+    (Shoe description color size (Data.Text.pack shoeImageFile))
+
+instance FromJSON ShoeRaw
+instance ToJSON ShoeRaw
+
+sourceByteString :: MonadIO m => ByteString -> Source m ByteString
+sourceByteString byteString = do
+    yield byteString
+
+fileNameFromBytes :: ByteString -> FilePath
+fileNameFromBytes bytes =
+    let hashString = B.unpack $ Data.ByteString.Base16.encode $ SHA256.hash bytes
+    in "./images/" ++ hashString ++ ".jpg"
+
+saveImageData :: Text -> IO String
+saveImageData imageData = do
+    let byteData = Data.ByteString.Base64.decode $ encodeUtf8 imageData
+    case byteData of
+        Right bytes -> do
+            let filePath = fileNameFromBytes bytes
+            runResourceT $ (sourceByteString bytes) $$ CB.sinkFile filePath
+            return filePath
+        _ -> do
+            print "Invalid data"
+            return ""
+
+queryShoe :: MonadIO m => Int64 -> SqlPersistT m (Maybe Shoe)
+queryShoe shoeId = do
+    let Right key = keyFromValues [PersistInt64 shoeId]
+    get key
+
---
+++ Server.hs
@@ -0,0 +1,115 @@
+{-# LANGUAGE TemplateHaskell, QuasiQuotes #-}
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
+import           Control.Exception        (SomeException)
+import           Control.Exception.Lifted (handle)
+import           Control.Monad.IO.Class   (MonadIO, liftIO)
+import           Data.Aeson               (Value, encode, decodeStrict, object, (.=) )
+import           Data.Aeson.Parser        (json)
+import           Data.ByteString          (ByteString, concat)
+import           Data.ByteString.Char8    (pack)
+import           Data.Conduit             (($$), Sink)
+import           Data.Conduit.Attoparsec  (sinkParser)
+import           Network.HTTP.Types       (status200, status400, status404)
+import           Network.Wai              (Application, Request, Response, responseLBS, pathInfo, responseBuilder, requestMethod, responseFile)
+import           Network.Wai.Conduit      (sourceRequestBody)
+import           Network.Wai.Handler.Warp (run)
+import           Blaze.ByteString.Builder (copyByteString)
+import           Data.Text                (Text, unpack, pack, append)
+import           Data.Serialize           (encode)
+import           Data.Monoid              (mconcat)
+import           Database.Persist.Class   (keyToValues)
+import           Data.Int                 (Int64)
+import           Text.Blaze.Html.Renderer.Utf8 (renderHtmlBuilder)
+import           Text.Hamlet
+import           Database.Persist.Sqlite
+import           GHC.Generics
+import qualified Data.Conduit.List as CL
+
+-- custom modules
+import           DataTypes                (Shoe, ShoeRaw, shoeFromRawData, saveImageData, photo, queryShoe)
+
+
+main :: IO ()
+main = run 3000 app
+
+getDefault :: Network.Wai.Request -> IO Response
+getDefault req = do
+    return $ responseLBS
+        status404
+        [("Content-Type", "application/json")]
+        $ "Not Found"
+
+data Route = GetShoes | GetShoe Int64
+
+renderUrl :: Route -> [(Text, Text)] -> Text
+renderUrl GetShoes _ = "/shoes"
+renderUrl (GetShoe shoeId) _ = Data.Text.append "/shoes/" (Data.Text.pack (show shoeId))
+
+shoesTemplate :: HtmlUrl Route
+shoesTemplate = [hamlet|
+<html>
+    <head>
+        shoes
+    <body>
+        shoes
+|]
+
+shoeTemplate :: Int64 -> HtmlUrl Route
+shoeTemplate shoeId = [hamlet|
+<html>
+    <head>
+        shoe
+    <body>
+        shoe
+|]
+
+getShoes :: Network.Wai.Request -> IO Response
+getShoes req = do
+    let html = shoesTemplate renderUrl
+    return $ responseBuilder status200 [ ("Content-Type", "text/plain") ] $ renderHtmlBuilder html
+
+getShoe :: Network.Wai.Request -> Int64 -> IO Response
+getShoe req shoeId = do
+    shoe <- runSqlite "shoes.db" $ queryShoe shoeId
+    let html = shoeTemplate shoeId renderUrl
+    return $ responseBuilder status200 [ ("Content-Type", "text/plain") ] $ renderHtmlBuilder html
+    --return $ responseFile  status200 [ ("Content-Type", "image/jpeg") ] "./images/image.jpg" Nothing
+
+aggregateSink :: MonadIO m => Sink ByteString m ByteString
+aggregateSink = CL.fold (\s1 s2 -> Data.ByteString.concat [s1,s2]) ""
+
+postShoes :: Network.Wai.Request -> IO Response
+postShoes req = do
+    shoe :: Maybe ShoeRaw <- (sourceRequestBody req $$ aggregateSink) >>= (return . decodeStrict)
+    case shoe of
+      Just rawData  -> do
+        imageFilePath <- saveImageData $ photo rawData
+        shoeId <- runSqlite "shoes.db" $ do
+            shoeId <- insert $ shoeFromRawData rawData imageFilePath
+            return shoeId
+        let Right (intValue :: Int64) = (fromPersistValue.head.keyToValues) shoeId
+        return $ responseBuilder status200 [ ("Content-Type", "text/plain") ] $ mconcat $ map copyByteString [ "post shoes ", Data.ByteString.Char8.pack (show intValue), "\n" ]
+
+      Nothing -> return $ responseBuilder status400 [ ("Content-Type", "text/plain") ] $ mconcat $ map copyByteString [ "Bad request" ]
+
+app :: Application
+app req sendResponse = handle (sendResponse . invalidJson) $ do
+    response <- case (requestMethod req, pathInfo req) of
+      ("GET",  ["shoes", shoeId]) -> getShoe req  (read ( Data.Text.unpack shoeId ))
+      ("GET",  ["shoes"]) -> getShoes req
+      ("POST", ["shoes"]) -> postShoes req
+      _                   -> getDefault  req
+
+    sendResponse response
+
+invalidJson :: SomeException -> Response
+invalidJson ex = responseLBS
+    status400
+    [("Content-Type", "application/json")]
+    $ Data.Aeson.encode $ object
+        [ ("message" .= show ex)
+        ]
+
+--{"description": "SADIE Faux Suede Heels with Bow", "color": "red", "size": 34, "photo": "/9j/4AAQSkZJRgABAQAAAQABAAD/2wBDAAMCAgMCAgMDAwMEAwMEBQgFBQQEBQoHBwYIDAoMDAsKCwsNDhIQDQ4RDgsLEBYQERMUFRUVDA8XGBYUGBIUFRT/2wBDAQMEBAUEBQkFBQkUDQsNFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBT/wAARCAA2ACUDASIAAhEBAxEB/8QAGwABAQACAwEAAAAAAAAAAAAAAAkGBwEFCAr/xAAxEAABAwMDAgUCBAcAAAAAAAABAgMEBQYRAAchCBIJEyIxUTJhF0GCkSMzQlJxgaH/xAAaAQABBQEAAAAAAAAAAAAAAAAAAgMEBQcG/8QAKxEAAQMDAgMHBQAAAAAAAAAAAQACAwQFERIhIjFxBhMyQVFhgaHB0eHx/9oADAMBAAIRAxEAPwCqClJQMqISPknXPGpgeKRvTNuC7U7a0+oPRaVTGG35jbKyA9KWAtPdj3CElGPglWs18N/ruO4UWHtTuJPIuyKnyaRVJCuai2kfyXFH3eSBwT9YGD6h6mhICcLpKqxVFLRw1bjnvBqx6D+YPQ+xVCuPjTj40xpjTq5tONNMaaEKSviRbJ1qxt3pd5+WqTb1xL8xmTyfKfCfWyo/keO5Pyk8fSceHno1RolYj1ykyHYEyM+mTGlMEpU08ghQUlQ9iDj/AJq3XiFW8mv9LtwKUgKXCmQZCCRkpJkttEj9LqtS3bsuVZN6XFt7c7PlS4z6h5CvoWsDlSPstHaoEe4xqvk4HEBbXaqxl5tbYp/HHt8AAA/UZ/aq10T9TUfqe2Zh1qQW2rpppEGuREcdr4HDoH5IcHqH5A9w/p1v/wDfUH+nPqCqnRrvy7Ukoen27KIi1enoIzJjE5S4gHjzEZ7h+pOQFHVtLD3YtHcyzIV123X4VSoMsJDctDwSErUQA2sHBQvJAKFYIJxjUuN+oLL7pQOo5yGjhPL8LLP300wTpp1Ui6K+bHo249qVG27hhmfR56AiRHDq2ioBQUMLQQpJCkgggg8anB4ou1kml3dQL8hxVR1ONCDKlMjAUpHLKyf7u3KT9ko1T3H21je4W3dB3StSbblyQG6jSpiO1xpfuPggjkEHkEcjSHsDhhXNpuLrZVsn5tGxHqDz+x6gL5/Kg63e1WpzNecDDaXAl2ayj+IGyecge/8Aof4B9tbAvna/8MaYmo27PcqNjTXEOImJX6mncHsRISDjux3dqx6VDPbg9yR7wvjwmLSqSlu2zdtWpKlZwxMSiQ2PsOEq/cnWvZvhLXfPhsQHNzWDT2HFOtsrgLUkKPurt80DP31BMMgIwdlpMfaW2RytqYTpd5gt8uozg/K9PeH1uBeO4uxDU+7JaaoyxKMal1IvIcekR0oTkOkKKu9CypHrCVYSD6shRa7/AKP+lxXSxZVWoi7nkXM7UpglrUqMIzTJCAnCG+9fJxyrPOE8DHLU9uQBlZhc5Yp6yWWHGlxyMDA39lvzA0wNNNKVYnGuONNNCEOmmmhC/9k="}
